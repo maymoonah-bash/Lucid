6c6e09afe20e72ab59c4202d5c40abbd3eb296f8
maymoonah bashir <68162866+maymoonah-bash@users.noreply.github.com>
2023-01-08 14:50:45 +0300

    Initial Commit

---
+++ Lucid/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Lucid
+//
+//  Created by Maimuna on 15/06/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lucid/LucidApp.swift
@@ -0,0 +1,17 @@
+//
+//  LucidApp.swift
+//  Lucid
+//
+//  Created by Maimuna on 15/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct LucidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
