7a87ee8e2eee38f326d11edbb8883cf6dca3eee4
maymoonah bashir <68162866+maymoonah-bash@users.noreply.github.com>
2023-01-09 20:46:29 +0300

    full code my part

--- Lucid/LucidApp.swift
+++ Lucid/LucidApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct LucidApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TextPage()
         }
     }
 }
---
+++ Lucid/TextPage.swift
@@ -0,0 +1,70 @@
+//
+//  ContentView.swift
+//  Lucid
+//
+//  Created by Maimuna on 15/06/1444 AH.
+//
+
+import SwiftUI
+
+struct TextPage: View {
+    @State private var content = ""
+    var body: some View {
+        VStack {
+            ZStack(alignment: .leading) {
+                TextEditor(text: $content)
+                    .font(.largeTitle)
+                    .padding(.top, 25)
+                    .padding(.leading, 10)
+                    .opacity(self.content.isEmpty ? 0.25 : 1)
+                
+                if content.isEmpty {
+                    Text("Tap to start typing")
+                        .font(.largeTitle)
+                        .foregroundColor(Color.gray)
+                        .padding(.top, 65)
+                        .padding(.top, -370)
+                        .padding(.leading, 14)
+                }
+            }
+            if content.isEmpty {
+                Button("Clear") {
+                    self.content = ""
+                }
+                .padding(.leading, 270)
+                
+                .buttonStyle(GrowingButton())
+            }
+        }.onTapGesture { hideKeyboard() }
+            .ignoresSafeArea(.keyboard)
+        
+    }
+}
+extension View {
+    func hideKeyboard() {
+        let resign = #selector(UIResponder.resignFirstResponder)
+        UIApplication.shared.sendAction(resign, to: nil, from: nil, for: nil)
+    }
+}
+struct CustomColor {
+    static let Text = Color("Text")
+    static let OurBlue = Color("OurBlue")
+    // Add more here...
+}
+struct GrowingButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(CustomColor.OurBlue)
+            .foregroundColor(Color.white)
+            .clipShape(Capsule())
+            .font(.system(size: 20))
+            .scaleEffect(configuration.isPressed ? 1.2 : 1)
+            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+    }
+}
+struct TextPage_Previews: PreviewProvider {
+    static var previews: some View {
+        TextPage()
+    }
+}
